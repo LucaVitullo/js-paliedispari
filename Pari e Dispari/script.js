4e3ffd296d2c38d625873f08391fd4a095666c2a
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-14 18:11:01 +0100

    esercizio 2 completato

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -7,3 +7,31 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
+
+const sceltaUtente =prompt("Scegli pari o dispari");
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+const numeroComputer= nRandom(1,5);
+let controllo =pariODispari (numeroComputer, numeroUtente);
+
+
+function nRandom(min,max){
+    const nRandom =Math.floor(Math.random()*max)+min;
+    return nRandom;
+}
+
+function pariODispari (numeroComputer, numeroUtente){
+    let somma= numeroUtente + numeroComputer;
+    console.log( "Tu hai inserito il numero " + numeroUtente, " ed il computer ha inserito il numero " + numeroComputer);
+    console.log("La somma è " + somma);
+    if(somma % 2 !== 0){
+        return 'dipari';
+    }else {
+        return 'pari';
+    }
+}
+
+if (controllo == sceltaUtente){
+    console.log("Hai vinto");
+}else{
+    console.log("Hai perso");
+}
