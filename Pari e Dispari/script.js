5baabb479f1fb2887a9710d1645d64900d1728f8
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-14 14:45:14 +0100

    inserimento file nelle cartelle apposite

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -1,19 +1,4 @@
 
-/////ESERCIZIO 1
-/*Palidroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma*/
-
-
-
-
-
-
-
-
-
-
-
 //////ESERCIZIO 2
 
 /*Pari e Dispari
---
+++ palindroma/script.js
@@ -0,0 +1,14 @@
+
+/////ESERCIZIO 1
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+
+
+const chiedoParola = prompt("inserisci una parola");
+
+
+
+
+
