7539ca382fb41e59d2ae6a05018d68dc595d376e
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-14 15:46:15 +0100

    primo esericzio completato

--- palindroma/script.js
+++ palindroma/script.js
@@ -5,10 +5,19 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 
 
+let parola = prompt("inserisci una parola");
 
-const chiedoParola = prompt("inserisci una parola");
+let parolaInversa = invertiParola(parola);
 
-
-
-
-
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  }else {
+    console.log('la parola non è palindroma');
+  }
+  
+function invertiParola(str){
+  let strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
+ console.log (parolaInversa);
+ console.log(parola)
